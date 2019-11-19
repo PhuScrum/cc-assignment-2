110701401d9d756ac0294e78e015bc96f9af1c6a
s3634996 <s3634996@rmit.edu.vn>
2019-11-19 23:20:22 +0700

    fixed sign up
    it now has authentication code

--- src/config.js
+++ src/config.js
@@ -5,8 +5,21 @@ export default {
     },
     cognito:{
         REGION:'us-east-1',
-        USER_POOL_ID:'us-east-1_AVkhlsemI',
-        APP_CLIENT_ID: '35c3p4oblr0rds72108e52h7bb',
-        IDENTITY_POOL_ID:'us-east-1:2d5215d7-2b60-400d-b8a5-b7cb5f4833c9'
+        USER_POOL_ID:'us-east-1_9yHEahPWy',
+        APP_CLIENT_ID: '1ttne14j3121llt108u9q9pco9',
+        IDENTITY_POOL_ID:'us-east-1:acf4cbb3-1e98-42e8-853d-cccded0ed0c8'
     }
     }
+    // export default {
+    //     apiGateway: {
+    //         REGION: 'us-east-1',
+    //         URL:'https://nx7eiabksh.execute-api.us-east-1.amazonaws.com/dev',
+    //     },
+    //     cognito:{
+    //         REGION:'us-east-1',
+    //         USER_POOL_ID:'us-east-1_AVkhlsemI',
+    //         APP_CLIENT_ID: '35c3p4oblr0rds72108e52h7bb',
+    //         IDENTITY_POOL_ID:'us-east-1:2d5215d7-2b60-400d-b8a5-b7cb5f4833c9'
+    //     }
+    //     }
+    
